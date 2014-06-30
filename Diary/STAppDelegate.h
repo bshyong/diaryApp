fc387c98f3360a55e97d9dd77fb224300df2e4f3
Benjamin Shyong <ben.shyong@gmail.com>
2014-06-30 01:54:41 -0700

    Initial Commit

---
+++ Diary/STAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  STAppDelegate.h
+//  Diary
+//
+//  Created by Benjamin Shyong on 6/30/14.
+//  Copyright (c) 2014 ShyongTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Diary/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Diary
+//
+//  Created by Benjamin Shyong on 6/30/14.
+//  Copyright (c) 2014 ShyongTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([STAppDelegate class]));
+  }
+}
