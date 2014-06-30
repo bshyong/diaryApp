6cd2e8f086a9eca6cde009ac46963bf2886afe5a
Benjamin Shyong <ben.shyong@gmail.com>
2014-06-30 13:49:22 -0700

    move core data stack to separate file

---
+++ Diary/STCoreDataStack.h
@@ -0,0 +1,22 @@
+//
+//  STCoreDataStack.h
+//  Diary
+//
+//  Created by Benjamin Shyong on 6/30/14.
+//  Copyright (c) 2014 ShyongTech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface STCoreDataStack : NSObject
+
++(instancetype)defaultStack;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
